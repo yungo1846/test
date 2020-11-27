bbd45e253dd23b8e72fa38591ac66294d8ad11dd
yungo1846 <yungo1468@naver.com>
2020-11-28 02:58:06 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,293 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Header from './Header'
+import Calendar from './Calendar'
+import delete_img from './style/delete.png';
+import { confirmAlert } from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-red-500">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import moment from 'moment'
+
+import './style/RCA.css'
+
+export default class App extends Component {
+    
+    state = {
+        calendarYM : moment(),
+        today : moment(),
+        selected: moment().format("YYYY-MM-DD"),
+        title: "",
+        end_date: "",
+        description: "",
+        id: 2, // 배포 시 schdule 리스트 비우고 id도 0으로 초기화 필요
+        company: ["삼산텍", "풀무원", "토스", "IBM", "삼성SDS"],
+        selectedCompany: "default",
+        schedule: [{ id: 0, title: "삼산텍 버그픽스", company: "삼산텍", date: "2020-11-27", end_date:"2020-11-30", description: "오후 5시까지 담당자 만날 것" }, { id: 1, title: "react 개발", company: "토스", date: "2020-11-27", description: "react hook 공부" }],
+        company_clicked: "전체",
+        del_clicked: false,
+        plus_clicked: true,
+        new_company: "",
+        company_del_clicked: false,
+    }
+
+    static defaultProps = {
+        clickFn : ()=>{}
+    }
+
+    moveMonth = (month) => {
+        this.setState({
+            calendarYM : this.state.calendarYM.add(month,'M')
+        })
+    }
+
+    changeSelected = (clickedDate) =>{
+
+        if(moment(clickedDate).isSame(this.state.selected,'day')){
+            this.props.clickFn(clickedDate);
+            return;
+        }
+
+        this.setState({
+            selected : clickedDate
+        })
+
+        this.props.clickFn(clickedDate)
+        
+        if(moment(clickedDate).isBefore(this.state.calendarYM,'month')){
+            this.moveMonth(-1)
+        }else if(moment(clickedDate).isAfter(this.state.calendarYM,'month')){
+            this.moveMonth(1)
+        }
+    }
+
+    submit = (e) => {
+        e.preventDefault()
+        if (this.state.selectedCompany !== "default") {
+            confirmAlert({
+              title: '삭제 확인',
+              message: '\''+this.state.selectedCompany+'\''+'을(를) 정말로 삭제 하시겠습니까?',
+              buttons: [
+                {
+                  label: 'Yes',
+                  onClick: () => this.handleCompanyDelete()
+                },
+                {
+                  label: 'No',
+                  onClick: () => alert('취소 되었습니다.')
+                }
+              ]
+            });
+        }
+      };
+
+    handleCompanyClicked = (e) => {
+        this.setState({
+          company_clicked: e.target.id,
+        }, () => console.log(this.state.company_clicked))
+      }
+      
+    handleCompanyDelClicked = (e) => {
+        const { company_del_clicked } = this.state;
+        this.setState({
+            company_del_clicked: !company_del_clicked,
+        },() => { console.log(this.state.company_clicked) })
+    }
+    
+    handleCompanyDelete = () => {
+        this.setState({
+            company: this.state.company.filter(company => company !== this.state.selectedCompany),
+            schedule: this.state.schedule.filter(schedule => schedule.company !== this.state.selectedCompany)
+        }, () => { console.log(this.state.company, this.state.schedule) })
+    }
+    
+    companyChange = (event) => {
+    this.setState({
+        new_company: event.target.value,
+    })
+    }
+
+    handleCompanySubmit = (event) => {
+    event.preventDefault()
+    if (this.state.new_company !== "") {
+        this.setState({
+        company: this.state.company.concat(this.state.new_company),
+        new_company: "",
+    }) 
+    }
+    event.target.reset()
+    }
+
+    titleChange = (e) => {
+        this.setState({
+            title: e.target.value,
+        })
+    }
+
+    endDateChange = (e) => {
+        this.setState({
+            end_date: e.target.value,
+        })
+    }
+
+    descriptionChange = (e) => {
+        this.setState({
+            description: e.target.value,
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        const { schedule, id, title, selectedCompany, description, end_date, selected } = this.state;
+        const endDate = moment(end_date);
+        const startDate = moment(selected);
+        var diff = endDate.diff(startDate, 'days');
+        var count;
+        var new_schedule = [];
+        for (count = 0; count <= diff; count++){
+            new_schedule.push({id: id, title: title, company: selectedCompany, date: startDate.clone().add(count, 'days').format("YYYY-MM-DD"), end_date: end_date, description: description})
+        }
+        console.log(new_schedule, count, diff)
+        this.setState({
+            schedule: schedule.concat(new_schedule),
+            id: id + 1,
+        }, () => console.log(this.state))
+        event.target.reset()
+    }
+
+    deleteSchedule = (e,id) => {
+        this.setState({
+            schedule: this.state.schedule.filter(schedule => schedule.id !== id)
+        })
+        console.log(id);
+    }
+
+    handleSelect = (e) => {
+        this.setState({
+            selectedCompany: e.target.value
+        })
+    }
+    
+    render() {
+        return (
+            <div>
+            <div className="entire flex justify-center items-center">
+            <div className="border border-black rounded-md w-1/6 input_box flex flex-col">
+          <div className="flex-1 ml-3 mt-5">
+                            <div className="font-bold text-3xl mb-3">회사별 일정</div>
+                            <div className="flex flex-row flex-wrap">
+                            <div className={"cursor-pointer border rounded-lg text-center bg-blue-300 w-2/6 h-10 text-xl font-medium mx-1 my-2 "+(this.state.company_clicked==="전체" ? 'btn_clicked' : '')} id="전체" onClick={this.handleCompanyClicked}>전체</div>
+            {this.state.company.map((company, i) => {
+              return(
+                  <div className={"cursor-pointer border rounded-lg text-center bg-blue-300 w-2/6 h-10 text-xl font-medium mx-1 my-2 "+(this.state.company_clicked===company ? 'btn_clicked' : '')} id={company} onClick={this.handleCompanyClicked}>{company}</div>)
+            })}
+                        </div>
+                            </div>
+                        <div className="flex-1">
+          <div className="ml-3 mt-5">
+          <div className="font-bold btn text-3xl">회사 추가</div>
+          <form onSubmit={this.handleCompanySubmit}>
+            <div className="flex flex-row">
+                            <input className="border border-black mt-2 h-10 w-4/6" type="text" id="new_company" value={this.new_company} onChange={this.companyChange}></input>
+                        <button className="border rounded-md w-16 font-bold text-2xl mt-1 ml-3 float-right text-center bg-blue-400">
+                                    저장
+                        </button>
+                        </div>
+                        </form>
+          </div>
+          <div className="ml-3 mt-5">
+                            <span className="cursor-pointer font-bold btn text-3xl">회사 삭제</span>
+                            <div className="flex flex-row"><form onSubmit={this.submit}><select name="del_company" className="mt-2 text-xl border border-black h-10" size="1" onChange={this.handleSelect}>
+                            <option value="default">회사를 선택하세요.</option>
+                                {this.state.company.map((company, i)=>{
+                                    return(
+                                        <option value={company}>{company}</option>
+                                    )
+                                })}
+                            </select>
+                            <button className="border rounded-md w-16 font-bold text-2xl mt-2 ml-5 float-right text-center bg-red-600">
+                            삭제
+                </button></form></div>
+                            </div>
+                            </div>
+        </div>
+                <div className="test-layout">
+                    <div className="RCA-app-container">
+                        <Header calendarYM={this.state.calendarYM.format("YYYY년 MM월")}
+                            today={this.state.today.format("오늘 YYYY - MM - DD")}
+                            moveMonth={this.moveMonth}
+                            />
+                            {this.state.company_clicked === "전체" ? <Calendar YM={this.state.calendarYM.format("YYYY-MM-DD")}
+                            selected={this.state.selected}
+                            changeSelected={this.changeSelected}
+                            schedule={this.state.schedule}
+                        /> : <Calendar YM={this.state.calendarYM.format("YYYY-MM-DD")}
+                        selected={this.state.selected}
+                        changeSelected={this.changeSelected}
+                        schedule={this.state.schedule.filter(schedule => schedule.company === this.state.company_clicked)}
+                    /> }
+                    </div>
+                </div>
+                <div className="input_box border rounded-md border-black w-1/5 flex flex-col">
+                    <div className="flex-1">
+                    <div className="font-bold text-4xl text-center mt-2">
+                        Work Calendar
+                    </div>
+                    <div className="mt-10 ml-3 font-bold text-4xl">
+                        {this.state.selected}
+                    </div>
+                    <div className="mt-5 mb-2 ml-5 font-bold text-2xl">
+                        일정
+                    </div>
+                    <div>
+                    {this.state.schedule.filter(schedule => (schedule.company===this.state.company_clicked) || this.state.company_clicked === "전체").map((info, i) => {
+                    if (this.state.selected === info.date) {
+                        return (
+                            <div className="break-all ml-8 text-xl font-bold">
+                                <div className="flex flex-row">- {info.title}
+                                    <button id={info.id} onClick={(e) =>this.deleteSchedule(e,info.id) }>
+                                        <img src={delete_img} className="mx-2 w-4 h-4"></img>
+                                        </button>
+                                    </div>
+                                <div className="break-all ml-2 mb-px font-medium">: {info.description}</div>
+                            </div>)
+                    }
+                    })}
+                        </div>
+                        </div>
+                    <div className="flex-1">
+                        <form onSubmit={this.handleSubmit}>
+                        <div className="font-bold ml-5 text-2xl">일정추가</div>
+                        <div className="mt-5 ml-8 font-bold text-xl">
+                        제목
+                        </div>
+                            <input className="border border-black mt-2 ml-8 w-5/6 h-8" type="text" id="title" value={this.title} onChange={this.titleChange} required></input>
+                            <div className="mt-5 ml-8 font-bold text-xl">
+                                회사
+                        </div>
+                                <select name="company" className="mt-2 ml-8 text-xl border border-black" size="1" onChange={this.handleSelect} required>
+                                <option value="">회사를 선택하세요.</option>
+                                {this.state.company.map((company, i)=>{
+                                    return(
+                                        <option value={company}>{company}</option>
+                                    )
+                                })}
+                                </select>
+                        <div className="mt-5 ml-8 font-bold text-xl">
+                                    종료 날짜
+                        </div>
+                                <input className="mt-2 ml-8 text-xl" type="date" id="date" value={this.end_date} onChange={this.endDateChange} required></input>
+                        <div className="mt-5 ml-8 font-bold text-xl">
+                            설명
+                        </div>
+                        <textarea className="border border-black mt-2 ml-8 w-5/6" rows="5" id="description" value={this.description} onChange={this.descriptionChange}></textarea>
+                        <br></br>
+                        <button className="border rounded-md w-16 font-bold text-2xl mt-10 mr-5 float-right text-center bg-blue-400">
+                                    저장
+                        </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+         </div>
+        )
+    }
 }
-
-export default App;
---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="RCA-header-container">
+                <h2 className="RCA-header-calendarYM RCA-header-middle font-bold text-3xl">
+                    {this.props.calendarYM}
+                </h2>
+                <h3 className="RCA-header-today RCA-header-middle font-bold text-2xl mr-5">
+                    {this.props.today}
+                </h3>
+                <ul className="RCA-header-buttons RCA-header-middle">
+                    <li>
+                        <i className="move-button left-img icon" onClick={()=>{this.props.moveMonth(-1)}}>
+
+                        </i>
+                    </li>
+                    <li className="text-xl font-bold">
+                        이동
+                    </li>
+                    <li>
+                        <i className="move-button right-img icon" onClick={()=>{this.props.moveMonth(1)}}>
+
+                        </i>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
